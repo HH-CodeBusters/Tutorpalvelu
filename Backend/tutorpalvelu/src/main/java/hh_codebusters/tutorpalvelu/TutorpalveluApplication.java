4e6f3c5ac2792b8b76a7bf2fad141c9a089ae45f
thelatez <lauri.e.rantala@gmail.com>
2026-01-22 13:43:34 +0200

    Create spring boot base with java version 25.

---
+++ Backend/tutorpalvelu/src/main/java/hh_codebusters/tutorpalvelu/TutorpalveluApplication.java
@@ -0,0 +1,13 @@
+package hh_codebusters.tutorpalvelu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorpalveluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorpalveluApplication.class, args);
+	}
+
+}
---
+++ Backend/tutorpalvelu/src/test/java/hh_codebusters/tutorpalvelu/TutorpalveluApplicationTests.java
@@ -0,0 +1,13 @@
+package hh_codebusters.tutorpalvelu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorpalveluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
