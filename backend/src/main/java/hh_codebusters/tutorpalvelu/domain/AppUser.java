747951ba3bf2a16c0b11bb571f68d8e03dfa72a6
Nicholas Mononen <nicholas.mononen@gmail.com>
2026-01-29 13:45:43 +0200

    AppUser ja sen repo tehty

---
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/domain/AppUser.java
@@ -0,0 +1,133 @@
+package hh_codebusters.tutorpalvelu.domain;
+
+public class AppUser {
+
+    private String firstname;
+    private String lastname;
+    private String phone;
+    private String email;
+    private String address;
+    private String zipcode;
+    private String city;
+    private String gender;
+    private String ssn;
+    private String school;
+    private boolean tutor;
+    private boolean parent;
+
+    public AppUser() {
+    }
+
+    public AppUser(String firstname, String lastname, String phone, String email,
+            String address, String zipcode, String city, String gender,
+            String ssn, String school, boolean tutor, boolean parent) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+        this.zipcode = zipcode;
+        this.city = city;
+        this.gender = gender;
+        this.ssn = ssn;
+        this.school = school;
+        this.tutor = tutor;
+        this.parent = parent;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public boolean isTutor() {
+        return tutor;
+    }
+
+    public void setTutor(boolean tutor) {
+        this.tutor = tutor;
+    }
+
+    public boolean isParent() {
+        return parent;
+    }
+
+    public void setParent(boolean parent) {
+        this.parent = parent;
+    }
+}
---
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/domain/AppUserRepository.java
@@ -0,0 +1,9 @@
+package hh_codebusters.tutorpalvelu.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppUserRepository extends CrudRepository<AppUser, Long> {
+
+}
