6c4c2844edc27ba2d587a166a4f2f12e3021cc8e
bhq718 <bhq718@myy.haaga-helia.fi>
2026-02-03 12:14:38 +0200

    poistettu reposta username

--- backend/src/main/java/hh_codebusters/tutorpalvelu/domain/AppUserRepository.java
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/domain/AppUserRepository.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    AppUser findByUsername(String username);
+ 
     List<AppUser> findByIsTutor();
 
 }
