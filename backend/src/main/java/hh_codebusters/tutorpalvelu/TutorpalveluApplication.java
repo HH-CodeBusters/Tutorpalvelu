071569837f9fb3acea63de6d549c51a2323de2fd
bhq718 <bhq718@myy.haaga-helia.fi>
2026-02-03 11:51:51 +0200

    lisätty h2 pommiin ja app propertiesiiin sekä testi tutor käyttäjä

--- backend/src/main/java/hh_codebusters/tutorpalvelu/TutorpalveluApplication.java
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/TutorpalveluApplication.java
@@ -1,13 +1,25 @@
 package hh_codebusters.tutorpalvelu;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import hh_codebusters.tutorpalvelu.domain.*;
 
 @SpringBootApplication
 public class TutorpalveluApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorpalveluApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo (AppUserRepository repository){
+		return (args) -> {
+			AppUser user1 = new AppUser("Tuomo", "Tutor", "+358415620247", "tuomo.tutor@gmail.com","Koulukatu 1 B 5", "00100", "Helsinki", "Male", "123234", "None", true, false);
+			repository.save(user1);
+			};
 	}
 
 }
