4ac0d23421bfd3ae5974f9e3db9eb7bf26a1f48a
bhq718 <bhq718@myy.haaga-helia.fi>
2026-02-10 11:54:04 +0200

    lisätty login.html, signup.html sekä appuser controller ja muokattu testidataa applicationissa

---
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/web/AppUserController.java
@@ -0,0 +1,68 @@
+package hh_codebusters.tutorpalvelu.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import jakarta.validation.*;
+
+import hh_codebusters.tutorpalvelu.domain.*;
+
+@Controller
+public class AppUserController {
+
+     //@Autowired
+private AppUserRepository repository;
+
+    public AppUserController(AppUserRepository repository) {
+        this.repository = repository;
+    }
+    
+    @RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }
+
+      @RequestMapping(value = "signup")
+    public String addStudent(Model model){
+    	model.addAttribute("signupform", new SignUpForm());
+        return "signup";
+    }
+
+     @RequestMapping(value = "saveuser", method = RequestMethod.POST)
+    public String save(@Valid @ModelAttribute("signupform") SignUpForm signupForm, BindingResult bindingResult) {
+    	if (!bindingResult.hasErrors()) { // validation errors
+    		if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match		
+	    		String pwd = signupForm.getPassword();
+		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+		    	String hashPwd = bc.encode(pwd);
+	
+		    	AppUser newUser = new AppUser();
+		    	newUser.setPasswordHash(hashPwd);
+		    	newUser.setEmail(signupForm.getEmail());
+		    	newUser.setRole("USER");
+		    	if (repository.findByEmail(signupForm.getEmail()) == null) { // Check if user exists
+		    		repository.save(newUser);
+		    	}
+		    	else {
+	    			bindingResult.rejectValue("username", "err.username", "Username already exists");    	
+	    			return "signup";		    		
+		    	}
+    		}
+    		else {
+    			bindingResult.rejectValue("passwordCheck", "err.passCheck", "Passwords does not match");    	
+    			return "signup";
+    		}
+    	}
+    	else {
+    		return "signup";
+    	}
+    	return "redirect:/login";    	
+    }    
+}
+
+
