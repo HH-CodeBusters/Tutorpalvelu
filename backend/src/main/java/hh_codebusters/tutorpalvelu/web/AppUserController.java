c6878857246553b1f1f832a9dd6e54ee737df8d6
Nicholas Mononen <nicholas.mononen@gmail.com>
2026-02-12 13:15:15 +0200

    Signup sivun pääsemiseen tehty korjaus

--- backend/src/main/java/hh_codebusters/tutorpalvelu/web/AppUserController.java
+++ backend/src/main/java/hh_codebusters/tutorpalvelu/web/AppUserController.java
@@ -15,54 +15,49 @@ import hh_codebusters.tutorpalvelu.domain.*;
 @Controller
 public class AppUserController {
 
-     //@Autowired
-private AppUserRepository repository;
+	// @Autowired
+	private AppUserRepository repository;
 
-    public AppUserController(AppUserRepository repository) {
-        this.repository = repository;
-    }
-    
-    @RequestMapping(value="/login")
-    public String login() {	
-        return "login";
-    }
+	public AppUserController(AppUserRepository repository) {
+		this.repository = repository;
+	}
 
-      @RequestMapping(value = "signup")
-    public String addStudent(Model model){
-    	model.addAttribute("signupform", new SignUpForm());
-        return "signup";
-    }
+	@RequestMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
 
-     @RequestMapping(value = "saveuser", method = RequestMethod.POST)
-    public String save(@Valid @ModelAttribute("signupform") SignUpForm signupForm, BindingResult bindingResult) {
-    	if (!bindingResult.hasErrors()) { // validation errors
-    		if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match		
-	    		String pwd = signupForm.getPassword();
-		    	BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
-		    	String hashPwd = bc.encode(pwd);
-	
-		    	AppUser newUser = new AppUser();
-		    	newUser.setPasswordHash(hashPwd);
-		    	newUser.setEmail(signupForm.getEmail());
-		    	newUser.setRole("USER");
-		    	if (repository.findByEmail(signupForm.getEmail()) == null) { // Check if user exists
-		    		repository.save(newUser);
-		    	}
-		    	else {
-	    			bindingResult.rejectValue("username", "err.username", "Username already exists");    	
-	    			return "signup";		    		
-		    	}
-    		}
-    		else {
-    			bindingResult.rejectValue("passwordCheck", "err.passCheck", "Passwords does not match");    	
-    			return "signup";
-    		}
-    	}
-    	else {
-    		return "signup";
-    	}
-    	return "redirect:/login";    	
-    }    
+	@RequestMapping(value = "/signup")
+	public String addStudent(Model model) {
+		model.addAttribute("signupform", new SignUpForm());
+		return "signup";
+	}
+
+	@RequestMapping(value = "saveuser", method = RequestMethod.POST)
+	public String save(@Valid @ModelAttribute("signupform") SignUpForm signupForm, BindingResult bindingResult) {
+		if (!bindingResult.hasErrors()) { // validation errors
+			if (signupForm.getPassword().equals(signupForm.getPasswordCheck())) { // check password match
+				String pwd = signupForm.getPassword();
+				BCryptPasswordEncoder bc = new BCryptPasswordEncoder();
+				String hashPwd = bc.encode(pwd);
+
+				AppUser newUser = new AppUser();
+				newUser.setPasswordHash(hashPwd);
+				newUser.setEmail(signupForm.getEmail());
+				newUser.setRole("USER");
+				if (repository.findByEmail(signupForm.getEmail()) == null) { // Check if user exists
+					repository.save(newUser);
+				} else {
+					bindingResult.rejectValue("username", "err.username", "Username already exists");
+					return "signup";
+				}
+			} else {
+				bindingResult.rejectValue("passwordCheck", "err.passCheck", "Passwords does not match");
+				return "signup";
+			}
+		} else {
+			return "signup";
+		}
+		return "redirect:/login";
+	}
 }
-
-
