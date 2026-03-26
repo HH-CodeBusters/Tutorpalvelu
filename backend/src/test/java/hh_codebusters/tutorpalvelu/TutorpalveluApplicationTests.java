55472f74925bcc5bdf335c6eab4ed624200800bf
bhq718 <bhq718@myy.haaga-helia.fi>
2026-03-26 11:14:24 +0200

    testi uusi käyttäjä backissä

--- backend/src/test/java/hh_codebusters/tutorpalvelu/TutorpalveluApplicationTests.java
+++ backend/src/test/java/hh_codebusters/tutorpalvelu/TutorpalveluApplicationTests.java
@@ -2,6 +2,7 @@ package hh_codebusters.tutorpalvelu;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import hh_codebusters.tutorpalvelu.domain.*;
 
 @SpringBootTest
 class TutorpalveluApplicationTests {
@@ -9,5 +10,43 @@ class TutorpalveluApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void addNewUser() {
+		// Testataan uuden käyttäjän luomista
+		AppUser user = new AppUser();
+		user.setEmail("testuser@mail.com");
+		user.setPasswordHash("password");
+		user.setFirstname("Test");
+		user.setLastname("User");
+		user.setRole("USER");
+		user.setPhone("+358415620247");
+		user.setAddress("Koulukatu 1 B 5");
+		user.setZipcode("00100");
+		user.setCity("Helsinki");
+		user.setGender("Male");
+		user.setSchool("Helsingin lukio");
+		user.setTutor(false);
+		user.setParent(false);
+
+		// Varmistetaan, että käyttäjään on asetettu oikeat arvot
+		assert user.getEmail().equals("testuser@mail.com");
+		assert user.getPasswordHash().equals("password");
+		assert user.getFirstname().equals("Test");
+		assert user.getLastname().equals("User");
+		assert user.getRole().equals("USER");
+		assert user.getPhone().equals("+358415620247");
+		assert user.getAddress().equals("Koulukatu 1 B 5");
+		assert user.getZipcode().equals("00100");
+		assert user.getCity().equals("Helsinki");
+		assert user.getGender().equals("Male");
+		assert user.getSchool().equals("Helsingin lukio");
+		assert user.isTutor() == false;
+		assert user.isParent() == false;
+
+	}
+
+	
+
 
 }
