ebd15e314273ab81b71a24b4542aaecef2e21c2a
TopiRiihimaki <topi.riihimaki@gmail.com>
2026-04-29 00:41:32 +0300

    Uusi testi luotu calendar.spec.ts

--- frontend/tutorpalvelureact/tests/calendar.spec.ts
+++ frontend/tutorpalvelureact/tests/calendar.spec.ts
@@ -9,3 +9,14 @@ test('Kalenteri page opens', async ({ page }) => {
 
 
 });
+/* Kun tämän testin ajaa, se luo aivan aidon ajan nimellä "Testiaika" tuotantosivulle. Siksi kommenteissa. Ajettu viimeksi 29/4/2026, toimi vielä silloin.
+test('User can add a new appointment', async ({page}) => {
+  await page.goto('https://tutorpalvelu.vercel.app/');
+  await page.getByRole('navigation').getByRole('link', { name: 'Kalenteri' }).click();
+  await page.locator('.fc-non-business').first().click();
+  await page.getByRole('textbox', { name: 'Varauksen otsikko' }).click();
+  await page.getByRole('textbox', { name: 'Varauksen otsikko' }).fill('Testiaika');
+  await page.getByRole('button', { name: 'Tallenna' }).click();
+  await expect(page.getByText('Testiaika')).toBeVisible
+});
+*/
