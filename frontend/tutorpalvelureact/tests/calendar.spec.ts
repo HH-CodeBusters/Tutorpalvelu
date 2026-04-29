5d9ad95283e1349ad069496226be66bb183ee003
TopiRiihimaki <topi.riihimaki@gmail.com>
2026-04-29 12:28:03 +0300

    Vielä yksi testi lisätty calendar tiedostoon

--- frontend/tutorpalvelureact/tests/calendar.spec.ts
+++ frontend/tutorpalvelureact/tests/calendar.spec.ts
@@ -19,4 +19,13 @@ test('User can add a new appointment', async ({page}) => {
   await page.getByRole('button', { name: 'Tallenna' }).click();
   await expect(page.getByText('Testiaika')).toBeVisible
 });
+
+Tämä testi poistaa aikasemmassa testissä luodun "Testiaika" ajan. Viimeksi ajettu 29/4/2026, toimi vielä silloin.
+test('User can delete the newly added appointment', async ({page}) =>{
+  await page.goto('https://tutorpalvelu.vercel.app/');
+  await page.getByRole('navigation').getByRole('link', { name: 'Kalenteri' }).click();
+  await page.locator('a').filter({ hasText: '- 15.00Testiaika' }).click();
+  await page.getByRole('button', { name: 'Poista' }).click();
+  await expect(page.getByText('Testiaika')).toHaveCount(0);
+});
 */
